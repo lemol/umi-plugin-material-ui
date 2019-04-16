d10432e3a1a093d758bcb7b753579f13acf242d0
Leza Lutonda (Lemol-C) <lemol-c@hotmail.com>
2019-04-17 00:46:23 +0100

    Add header tags

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@
 // - https://umijs.org/plugin/develop.html
 
 export default function (api, options) {
+  api.addHTMLMeta({
+    name: 'viewport',
+    content: 'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no',
+  });
 
-  // Example: output the webpack config
-  api.chainWebpackConfig(config => {
-    console.log(config.toString());
+  api.addHTMLLink({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500',
+  });
+
+  api.addHTMLLink({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
   });
 }
